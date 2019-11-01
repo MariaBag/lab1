410cd122df319ccfbfe3a7589b165a6aed72494e
MariaBag <43343526+MariaBag@users.noreply.github.com>
2019-11-01 23:21:46 +0500

    Task 7
    Task 7

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -3,15 +3,25 @@
 #include "buzzer.h"
 
 #define PIN_BUZZER 6
-#define PIN_BUTTON_OFF 5
+#define PIN_INCREASE_SPEED 5
+#define PIN_DECREASE_SPEED 4
+#define PIN_INCREASE_STEP 3
+#define PIN_DECREASE_STEP 2
 
-Button buttonOff(PIN_BUTTON_OFF);
+Button buttonIncreaseSpeed(PIN_INCREASE_SPEED);
+Button buttonDecreaseSpeed(PIN_DECREASE_SPEED);
+Button buttonIncreaseStep(PIN_INCREASE_STEP);
+Button buttonDecreaseStep(PIN_DECREASE_STEP);
 Buzzer buzzer(PIN_BUZZER);
 
-
-int notes[] = {NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_DS3, NOTE_SILENCE};
-double durations[] = {8, 8, 1, 8, 1, 8, 1, 24};
-int melodyLength = 8;
+int melodyStep = 1;
+int notes[] = {NOTE_G3, NOTE_E3, NOTE_E3, NOTE_G3, NOTE_E3, NOTE_E3, NOTE_G3, NOTE_F3, NOTE_E3, NOTE_D3, NOTE_C3, NOTE_SILENCE, 
+    NOTE_A3, NOTE_C4, NOTE_A3, NOTE_G3, NOTE_E3, NOTE_E3, NOTE_G3, NOTE_F3, NOTE_E3, NOTE_D3, NOTE_C3, NOTE_SILENCE, 
+    NOTE_A3, NOTE_C4, NOTE_A3, NOTE_G3, NOTE_E3, NOTE_E3, NOTE_G3, NOTE_F3, NOTE_E3, NOTE_D3, NOTE_C3, NOTE_SILENCE};
+double durations[] = {8, 4, 4, 8, 4, 4, 4, 4, 4, 4, 8, 8, 
+    8, 4, 4, 8, 4, 4, 4, 4, 4, 4, 8, 8, 
+    8, 4, 4, 8, 4, 4, 4, 4, 4, 4, 8, 8};
+int melodyLength = 36;
 
 void setup() {
     buzzer.setMelody(notes, durations, melodyLength);
@@ -19,10 +29,27 @@ void setup() {
 }
 
 void loop() {
-  
     buzzer.playSound();
-    if (buttonOff.wasPressed())
+    if (buttonIncreaseSpeed.wasPressed())
     {
-        buzzer.turnSoundOff();
+        for (int i = 0; i < melodyLength; i++) {
+          durations[i] /= melodyStep;
+        }
+        buzzer.setMelody(notes, durations, melodyLength);
+    }
+    if (buttonDecreaseSpeed.wasPressed())
+    {
+        for (int i = 0; i < melodyLength; i++) {
+          durations[i] *= melodyStep;
+        }
+        buzzer.setMelody(notes, durations, melodyLength);
+    }
+    if (buttonIncreaseStep.wasPressed())
+    {
+        melodyStep++;
+    }
+    if (buttonDecreaseStep.wasPressed())
+    {
+        melodyStep--;
     }
 }
